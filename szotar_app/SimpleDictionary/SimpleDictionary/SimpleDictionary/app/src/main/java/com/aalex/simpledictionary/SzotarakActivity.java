bac97349fa8ae70b01915dd32160e50bd5c8bfa3
aalex94 <apjok.alex17@gmail.com>
2015-12-07 14:35:32 +0100

    Javítva a kezdőképernyős felcserélődés. Alex

---
+++ szotar_app/SimpleDictionary/SimpleDictionary/SimpleDictionary/app/src/main/java/com/aalex/simpledictionary/SzotarakActivity.java
@@ -0,0 +1,16 @@
+package com.aalex.simpledictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by apjok on 2015. 11. 27..
+ */
+public class SzotarakActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.szotarak_layout);
+    }
+}
+
