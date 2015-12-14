ad8dc187369b7df647ed6d8b75aa4178a9573872
aalex94 <apjok.alex17@gmail.com>
2015-12-14 11:25:29 +0100

    Bővítettem az olasz listát. Ádám

--- szotar_app/SimpleDictionary/app/src/main/java/com/aalex/simpledictionary/MagyarOlasz.java
+++ szotar_app/SimpleDictionary/app/src/main/java/com/aalex/simpledictionary/MagyarOlasz.java
@@ -30,10 +30,14 @@ public class MagyarOlasz extends AppCompatActivity     {
         lv = (ListView)findViewById(R.id.magyarOlaszLista);
         sv=(SearchView)findViewById(R.id.searchView1);
         //
-        String[] values = new String[] {"A-Á", "", "a - the", "abbahagy - smettere", "abbamarad - cessare", " ABC - supermercato", "ablak - finestra", "ablaktörlő - tergicristallo", "abortusz - aborto",
-                "ábra - cifra", "ábránd - sognare", "ábrázol - rappresentare", "abrosz - tovaglia", "abszurd - assurdo", "acél - acciaio", "ács - falegname", "ácsorog - salotto", "ad - dare", "adat - dati", "ádáz - feroce",
-                "addig - prima", "adó - tasse", "adódik  - dovuto", "aggaszt - preoccuparsi", "ágy - letto", "agyag - argilla", "ajak - labbro", "ajándék - regalo", "ajándékoz - donare", "ajánlat - offerta", "ajtó - porta", "ajtócsengő - Chime", "akadály - barriera", "akár - anche", "állam - stato", "átlatlános - generale", "analizál - analizzare",
-                "anya - madre", "apa- padre", "aranyos - bello", "arc - faccia", "árny - ombra", "autó - auto", "autómata - automatici", };
+        String[] values = new String[] {"A-Á", "", "a - il","abbahagy - smettere", "abbamarad - cessare", " ABC - supermercato", "ablak - finestra", "ablaktörlő - tergicristallo", "abortusz - aborto", "ábra - cifra",
+                "ábránd - sognare", "ábrázol - rappresentare", "abrosz - tovaglia", "abszurd -assurdo", "acél - acciaio", "ács - falegname", "ácsorog - salotto", "ad - dare", "adat - dati", "ádáz - feroce", "addig - prima", "adó - tasse", "adódik  - dovuto", "aggaszt - preoccuparsi", "ágy - letto",
+                 "agyag - argilla", "ajak - labbro", "ajándék - regalo", "ajándékoz - donare", "ajánlat - offerta", "ajtó - porta", "ajtócsengő - Chime", "akadály - barriera", "akár - anche", "állam - stato", "átlatlános - generale",
+                "analizál - analizzare", "anya - madre", "apa- padre", "aranyos - bello", "arc - faccia", "árny - ombra", "autó - auto", "autómata - automatici", "B", "",  "bab - fagiolo", "baba - bambino", "baj - problemi",
+                "bajnok - campione", "bajnokság - campione", "bájos - attraente", "bakancs - avvio", "bal - sinistra", "baleset - incidente", "balta - ascia", "bámul - fissare", "bárhol - dovunque", "bárki - qualcuno",
+                "bármi - nulla", "bármikor - ogni volta che", "barna - colore marrone", "basszus - basso", "be - in", "beállít - impostato", "becsület - onore", "becsületes - onesto", "bedug - spina", "befagy - congelare",
+                "befed - coprire", "befejez - coprire", "belerak - metti dentro", "bizakodás - speranza", "bízik - credere in","bizonyít - dimostrare", "biztonság - sicurezza", "blöff - bluff", "bocsánatkérés - chiedere scusa",
+                "bogár - insetto", "bomba - bomba", "bolt - negozio", "bor - vino", "borda - costola", "borotva - rasoio", "bors - Pepe", "büszke - orgoglio", };
 
         final ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < values.length; ++i) {
